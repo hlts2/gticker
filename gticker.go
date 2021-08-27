4406e807c4244bf13f1799900b993c57e2e2301d
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2021-08-27 21:08:48 +0900

    add test of ticker
    Signed-off-by: hlts2 <hiroto.funakoshi.hiroto@gmail.com>

--- gticker.go
+++ gticker.go
@@ -5,32 +5,38 @@ import (
 	"time"
 )
 
-type InstantTicker struct {
+type InstantTicker interface {
+	Stop()
+	Reset(d time.Duration)
+	C() <-chan time.Time
+}
+
+type instantTicker struct {
 	tic        *time.Ticker
 	tch        chan time.Time
 	instTicked int32
 	stopped    int32
 }
 
-func NewInstantTicker(d time.Duration) *InstantTicker {
-	return &InstantTicker{
+func NewInstantTicker(d time.Duration) InstantTicker {
+	return &instantTicker{
 		tic: time.NewTicker(d),
 		tch: make(chan time.Time, 1),
 	}
 }
 
-func (it *InstantTicker) Stop() {
+func (it *instantTicker) Stop() {
 	it.tic.Stop()
 	if atomic.CompareAndSwapInt32(&it.stopped, 0, 1) {
 		close(it.tch)
 	}
 }
 
-func (it *InstantTicker) Reset(d time.Duration) {
+func (it *instantTicker) Reset(d time.Duration) {
 	it.tic.Reset(d)
 }
 
-func (it *InstantTicker) C() <-chan time.Time {
+func (it *instantTicker) C() <-chan time.Time {
 	if atomic.CompareAndSwapInt32(&it.instTicked, 0, 1) {
 		if atomic.LoadInt32(&it.stopped) != 1 {
 			it.tch <- time.Now()
